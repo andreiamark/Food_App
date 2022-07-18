00fe01292ff654774e0168ae3284c7f7d0477090
andreiamark <markovicanandrea@gmail.com>
2022-07-18 14:19:21 +0200

    Second commit

---
+++ food_app/lib/screens/categories_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/dummy_data.dart';
+import '../dummy_data.dart';
+import'../widgets/category_item.dart';
+
+class CategoriesScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+          padding: const EdgeInsets.all(25),
+          children: DUMMY_CATEGORIES
+           .map(
+              (catData) => CategoryItem(
+                catData.title as String,
+                catData.color as Color,
+                catData.id as String,
+          ),
+      )
+        .toList(),
+    gridDelegate:SliverGridDelegateWithMaxCrossAxisExtent(
+      maxCrossAxisExtent: 200,
+      childAspectRatio: 3 / 2,
+      crossAxisSpacing: 20,
+      mainAxisSpacing: 20,
+    ),
+      );
+
+  }
+}
+
